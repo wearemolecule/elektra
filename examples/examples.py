40a12e72f4d9f8118e1b638d67854195d0c07ca7
Sameer Soleja <sameer@molecule.io>
2021-03-12 13:33:52 -0600

    add runnable examples

---
+++ examples/examples.py
@@ -0,0 +1,43 @@
+import elektra
+import pandas as pd
+import filecmp
+import datetime as dt
+
+flow_date = dt.datetime(2020, 10, 17)
+
+### Create Prices (make a daily from lmps)
+print('\n\n--- Create Block Prices ---')
+prices = pd.read_csv('lmps.csv')
+#result = elektra.create_prices(dt.datetime.strptime('2020-10-17','%Y-%m-%d'),'M.P4F8', 'INDIANA.HUB', elektra.Iso.MISO, elektra.Block._2x16, 'Daily', prices)
+result = elektra.create_prices(flow_date, 'M.P4F8', 'INDIANA.HUB', 'miso', '2x16', 'Daily', prices)
+print(result)
+
+#### Scrub Hourly Prices (make sure there are enough houlies)
+print('\n\n--- Scrub Hourly Prices ---')
+result = elektra.scrub_hourly_prices(flow_date,'M.YERX', '116013753', 'pjm', prices)
+print(result)
+
+### Convert 
+print('\n\n--- Convert Blocks ---')
+result = elektra.convert(flow_date, '7x24', '2x16') # 16: (October 17 2020 is a Saturday, and has 16 peak hours)
+print(result)
+
+result = elektra.convert(flow_date, '7x24', '5x16') # 0: (October 17 2020 is a Saturday, and has 0 weekday peak hours)
+print(result)
+
+result = elektra.convert(flow_date, '5x16', '2x16') # 0: (October 17 2020 is a Saturday, and there could not be a 5x16 input block)
+print(result)
+
+### Translate Blocks
+print('\n\n--- Translate Blocks ---')
+flow_date = dt.datetime(2020, 10, 1)
+result = elektra.translateBlocks('pjm', 20, 'monthly', flow_date, '7x24', ['5x16', '2x16'], 'mwh')
+print(result)
+
+### Is DST Transition?
+print('\n\n--- DST Transition ---')
+flow_date = dt.datetime(2021, 3, 14)
+
+short_day, long_day = elektra.is_dst_transition(flow_date)
+print(short_day) # True; this is the sprint DST transition date
+print(long_day) # False; that would be the "fall back" date
