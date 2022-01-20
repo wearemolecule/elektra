0f8ce9783e4daf2f4df5e4c05f0c02e42cf13b1a
Paul <paul@molecule.io>
2022-01-20 17:16:51 -0600

    new version of elektra v0.0.26

---
+++ setup.py
@@ -0,0 +1,27 @@
+# https://www.blog.pythonlibrary.org/2021/09/23/python-101-how-to-create-a-python-package/
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="elektra",
+    version="0.0.26",
+    author="Molecule",
+    author_email="dev@molecule.io",
+    description="Power block price creation and conversion",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wearemolecule/elektra",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+     "pandas",
+     "numpy"   
+    ]
+)
