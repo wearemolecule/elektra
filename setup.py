5bf79d3ffca8bb67ea728059cef8116468007152
Paul <paul@molecule.io>
2023-03-02 09:23:09 -0600

    new pypi package version and requirements updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="elektra",
-    version="0.0.27",
+    version="0.0.28",
     author="Molecule",
     author_email="dev@molecule.io",
     description="Power block price creation and conversion",
