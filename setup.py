dc231e03f7ba476e1c7dab872459f1f29cc2a5c3
Paul <paul@molecule.io>
2023-04-11 14:24:17 -0500

    updated elektra pypi version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="elektra",
-    version="0.0.28",
+    version="0.0.29",
     author="Molecule",
     author_email="dev@molecule.io",
     description="Power block price creation and conversion",
